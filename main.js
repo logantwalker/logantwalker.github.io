21d12974cc143e62e9c8df3e2cc82e414d568ce3
L.Walker <logantwalker95@gmail.com>
2020-08-02 17:58:03 -0400

    greeting page testing

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        maxParticles: 100,
+        color: ['#a8e6cf', '#ffffff', '#ff8b94',],
+        connectParticles: true
+    });
+  };
