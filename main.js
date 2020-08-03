1df2c90d795397d27e87a36cbefc8f3ba6f241b1
L.Walker <logantwalker95@gmail.com>
2020-08-03 14:40:47 -0400

    ScrollReveal and nav features added

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ window.onload = function() {
         color: ['#a8e6cf', '#ffffff', '#ff8b94',],
         connectParticles: true
     });
+
+    ScrollReveal({duration:1000}).reveal('#greeting-parent',{delay: 500})
+
   };
 
 $('#custom-btn').click(()=>{
-    $('html,body').animate({
-        scrollTop: $(".nav").offset().top},
-        'slow');
+    $('html,body').animate({scrollTop: $(".nav").offset().top},'slow');
+    ScrollReveal({duration:1000}).reveal('.nav')
 })
+
+function showNavBar(){
+    let distance = $('.nav').offset().top;
+    console.log(distance);
+}
